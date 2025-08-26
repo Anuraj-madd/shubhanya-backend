0b1baed0822a8c707128b1ec7aeb59214bb351c9
Anuraj Maddhesiya <143344749+Anuraj-madd@users.noreply.github.com>
2025-08-26 15:09:25 +0530

    Refactor database connection to use PDO
    Updated database connection method to use PDO with SSL settings.

--- config.php
+++ config.php
@@ -1,13 +1,21 @@
 <?php
-$host = getenv("host");
-$db_name = getenv("db_name");
-$username = getenv("username"); // Default for XAMPP or Laragon
-$password = getenv("password");     // Default password is empty
 
-$conn = new mysqli($host, $username, $password, $db_name);
+$uri = getenv("uri");
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$fields = parse_url($uri);
+
+// build the DSN including SSL settings
+$conn = "mysql:";
+$conn .= "host=" . $fields["host"];
+$conn .= ";port=" . $fields["port"];;
+$conn .= ";dbname=defaultdb";
+$conn .= ";sslmode=verify-ca;sslrootcert=ca.pem";
+
+try {
+  $db = new PDO($conn, $fields["user"], $fields["pass"]);
+
+  $stmt = $db->query("SELECT VERSION()");
+  print($stmt->fetch()[0]);
+} catch (Exception $e) {
+  echo "Error: " . $e->getMessage();
 }
-?>
