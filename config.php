0f62b4ed26e759cd4f66ef3620d24ee903e69734
Anuraj Maddhesiya <143344749+Anuraj-madd@users.noreply.github.com>
2025-08-26 21:15:01 +0530

    Update config.php

--- config.php
+++ config.php
@@ -19,5 +19,5 @@ if (!mysqli_real_connect($conn, $host, $username, $password, $database, $port, N
     die("Connection failed: " . mysqli_connect_error());
 }
 
-echo "Connected successfully (via SSL)!";
+//echo "Connected successfully (via SSL)!";
 ?>
